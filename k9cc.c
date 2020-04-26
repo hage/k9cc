f759d4529094a28491c3bb920996791cac420e15
URA, Takefumi <ura.takefumi@gmail.com>
2020-04-26 16:09:24 +0900

    [refactor] アセンブラの定型文を別関数に

--- k9cc.c
+++ k9cc.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_header(void) {
+  printf(".intel_syntax noprefix\n");
+  printf(".global main\n");
+  printf("main:\n");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
 
-  printf(".intel_syntax noprefix\n");
-  printf(".global main\n");
-  printf("main:\n");
+  print_header();
+
   printf("        mov rax, %d\n", atoi(argv[1]));
   printf("        ret\n");
   return 0;
