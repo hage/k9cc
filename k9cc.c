3e01a986f00e8463e62976247cb1c680bdd1b493
URA, Takefumi <ura.takefumi@gmail.com>
2020-04-26 16:24:14 +0900

    [refactor] code出力関数cprintf
    * インデント
    * 改行
    
    を行う

--- k9cc.c
+++ k9cc.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 void print_header(void) {
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
+}
+
+void cprintf(const char *fmt, ...) {
+  printf("        ");
+  va_list ap;
+  va_start(ap, fmt);
+  vprintf(fmt, ap);
+  va_end(ap);
+  printf("\n");
 }
 
 int main(int argc, char **argv) {
@@ -15,7 +25,7 @@ int main(int argc, char **argv) {
 
   print_header();
 
-  printf("        mov rax, %d\n", atoi(argv[1]));
-  printf("        ret\n");
+  cprintf("mov rax, %d", atoi(argv[1]));
+  cprintf("ret");
   return 0;
 }
