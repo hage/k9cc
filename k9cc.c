e99e023188412d4e1688d5d603ad78cdfd4acb4d
URA, Takefumi <ura.takefumi@gmail.com>
2020-08-16 16:15:04 +0900

    [refactor] コードを出力する関数emit

--- k9cc.c
+++ k9cc.c
@@ -1,15 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+void emit(const char *fmt, ...) {
+  FILE *fpout = stdout;
+  size_t len = strlen(fmt);
+
+  if (*fmt != '.' && (0 < len && fmt[len - 1] != ':')) {
+    // ラベルなどでない通常のコードのとき
+    fprintf(fpout, "        ");
+  }
+
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(fpout, fmt, ap);
+  va_end(ap);
+  fprintf(fpout, "\n");
+
+}
+
+void emit_head(void) {
+  emit(".intel_syntax noprefix");
+  emit(".globl main");
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
     fputs("引数の個数が正しくありません\n", stderr);
     return 1;
   }
-  printf(".intel_syntax noprefix\n");
-  printf(".globl main\n");
-  printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
-  printf("  ret\n");
+  emit_head();
+  emit("main:");
+  emit("mov rax, %d", atoi(argv[1]));
+  emit("ret");
   return 0;
 }
